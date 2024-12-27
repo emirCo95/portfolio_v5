22fb8cec09dd392bbae6f12d4451a8e213c90db0
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-12-26 19:18:09 -0500

    text appear animation

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,16 +1,57 @@
-import React from 'react';
+'use client';
+import splitStringUsingRegex from '../utils/splitStringUsingRegex';
+import { motion } from 'framer-motion';
 
 const heading = 'Hi, I am Emir!';
 const text_1 = 'A Frontend Developer';
 const text_2 = 'Welcome to my portfolio!';
 
+const charVariants = {
+  hidden: { opacity: 0 },
+  reveal: { opacity: 1 },
+};
+
 const Hero = () => {
+  const headingChars = splitStringUsingRegex(heading);
+  const text_1_Chars = splitStringUsingRegex(text_1);
+
+  console.log(headingChars);
+
   return (
     <div className="w-full h-[90vh] flex flex-col md:flex-row items-center bg-hero-pattern bg-cover bg-no-repeat">
       <div className="w-full h-full flex flex-col items-center justify-center gap-2">
-        <h1 className="text-6xl md:text-9xl text-center">{heading}</h1>
-        <p className="text-4xl md:text-7xl text-center">{text_1}</p>
-        <p className="text-2xl md:text-4xl text-center mt-10">{text_2}</p>
+        <motion.h1
+          initial="hidden"
+          whileInView="reveal"
+          transition={{ staggerChildren: 0.09 }}
+          className="text-6xl md:text-9xl text-center"
+        >
+          {headingChars.map((char, index) => (
+            <motion.span
+              key={index}
+              transition={{ duration: 0.5 }}
+              variants={charVariants}
+            >
+              {char}
+            </motion.span>
+          ))}
+        </motion.h1>
+        <motion.p
+          initial="hidden"
+          whileInView="reveal"
+          transition={{ staggerChildren: 0.09 }}
+          className="text-4xl md:text-7xl text-center mt-10"
+        >
+          {text_1_Chars.map((char, index) => (
+            <motion.span
+              key={index}
+              transition={{ duration: 0.5 }}
+              variants={charVariants}
+            >
+              {char}
+            </motion.span>
+          ))}
+        </motion.p>
       </div>
     </div>
   );
