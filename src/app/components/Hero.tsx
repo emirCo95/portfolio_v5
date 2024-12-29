aac6e5ee6064a0df16c5346406eb1ad6ca2d500d
Emir Cokrlija <144956911+emirCo95@users.noreply.github.com>
2024-12-29 12:12:45 -0500

    minor updates

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,10 +1,10 @@
 'use client';
+import Image from 'next/image';
 import splitStringUsingRegex from '../utils/splitStringUsingRegex';
 import { motion } from 'framer-motion';
 
 const heading = 'Hi, I am Emir!';
 const text_1 = 'A Frontend Developer';
-const text_2 = 'Welcome to my portfolio!';
 
 const charVariants = {
   hidden: { opacity: 0 },
@@ -15,16 +15,14 @@ const Hero = () => {
   const headingChars = splitStringUsingRegex(heading);
   const text_1_Chars = splitStringUsingRegex(text_1);
 
-  console.log(headingChars);
-
   return (
     <div className="w-full h-[90vh] flex flex-col md:flex-row items-center bg-hero-pattern bg-cover bg-no-repeat">
-      <div className="w-full h-full flex flex-col items-center justify-center gap-2">
+      <div className="w-full md:w-[50%] h-full flex flex-col items-center justify-center gap-2">
         <motion.h1
           initial="hidden"
           whileInView="reveal"
           transition={{ staggerChildren: 0.09 }}
-          className="text-6xl md:text-9xl text-center"
+          className="text-6xl lg:text-9xl text-center"
         >
           {headingChars.map((char, index) => (
             <motion.span
@@ -40,7 +38,7 @@ const Hero = () => {
           initial="hidden"
           whileInView="reveal"
           transition={{ staggerChildren: 0.09 }}
-          className="text-4xl md:text-7xl text-center mt-10"
+          className="text-4xl lg:text-7xl text-center mt-10"
         >
           {text_1_Chars.map((char, index) => (
             <motion.span
@@ -52,6 +50,17 @@ const Hero = () => {
             </motion.span>
           ))}
         </motion.p>
+      </div>
+      <div className="w-full md:w-[50%] flex items-center justify-center border-2 border-black py-10 rounded-md">
+        <div className="w-[300px] h-[520px]">
+          <Image
+            className="w-full h-full shadow-[rgba(6,_24,_44,_0.4)_0px_0px_0px_2px,_rgba(6,_24,_44,_0.65)_0px_4px_6px_-1px,_rgba(255,_255,_255,_0.08)_0px_1px_0px_inset] rounded-md"
+            src="/hero.png"
+            alt="hero image"
+            width={300}
+            height={300}
+          />
+        </div>
       </div>
     </div>
   );
--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
@@ -5,12 +6,24 @@ const Navbar = () => {
     <div className="w-full h-[10vh] flex items-center justify-between">
       <span className="pl-10">Emir Cokrlija</span>
       <ul className="md:w-[60%] md:flex items-center justify-evenly">
-        <li>Home</li>
-        <li>About</li>
-        <li>Experience</li>
-        <li>Education</li>
-        <li>Skills</li>
-        <li>Projects</li>
+        <li>
+          <Link href="#home">Home</Link>
+        </li>
+        <li>
+          <Link href="#about">About</Link>
+        </li>
+        <li>
+          <Link href="#experience">Experience</Link>
+        </li>
+        <li>
+          <Link href="#education">Education</Link>
+        </li>
+        <li>
+          <Link href="#skills">Skills</Link>
+        </li>
+        <li>
+          <Link href="#projects">Projects</Link>
+        </li>
       </ul>
     </div>
   );
